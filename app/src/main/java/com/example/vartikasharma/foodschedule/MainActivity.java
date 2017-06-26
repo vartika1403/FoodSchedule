1cceda5a69fd424d8f97a882d31a9c0e3ab87ab9
vartika1403 <vartika.1403@gmail.com>
2017-06-26 16:33:24 +0530

    added timepicker and layout done

--- app/src/main/java/com/example/vartikasharma/foodschedule/MainActivity.java
+++ app/src/main/java/com/example/vartikasharma/foodschedule/MainActivity.java
@@ -1,11 +1,36 @@
 package com.example.vartikasharma.foodschedule;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 
+@RequiresApi(api = Build.VERSION_CODES.N)
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String[] mealList = {"Breakfast", "MorningSnack", "Lunch", "Evening Snack", "Dinner"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +38,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        createALayoutForMeals();
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void createALayoutForMeals() {
+        for (String item : mealList) {
+            LayoutInflater vi = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = vi.inflate(R.layout.meal_item_layout, null);
+            final TextView mealText = (TextView) v.findViewById(R.id.meal_name_text);
+            mealText.setText(item);
+            final TextView mealTime = (TextView) v.findViewById(R.id.meal_time_text);
+            mealTime.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Calendar currentTime = Calendar.getInstance();
+                    int hour = currentTime.get(Calendar.HOUR_OF_DAY);
+                    int minute = currentTime.get(Calendar.MINUTE);
+                    TimePickerDialog timePickerDialog;
+                    timePickerDialog = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                        @Override
+                        public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                            mealTime.setText(selectedHour + ":" + selectedMinute);
+                            Log.i(LOG_TAG, "selected hour, " + selectedHour);
+                        }
+                    }, hour, minute, false);
+                    timePickerDialog.setTitle("Select Time");
+                    timePickerDialog.show();
+                }
+            });
+            ViewGroup insertPoint = (ViewGroup) findViewById(R.id.main_layout);
+            insertPoint.addView(v, 0, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        }
     }
 }
